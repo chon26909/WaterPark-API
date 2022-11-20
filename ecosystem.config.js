87e849ebc0ed14ade2c4af43a0ae9d7f30166859
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-11-21 00:09:46 +0700

    change env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
                 PORT: 4001,
                 MONGO_URI:
                     'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/DEV?retryWrites=true&w=majority',
+                JWT_SECRET: waterpark,
             },
             env_uat: {
                 NODE_ENV: 'uat',
