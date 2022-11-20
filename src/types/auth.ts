b81b600059658d80fa4dc78e8cb831fbd84dc761
Chonlasit <chonlasit.s@arise.tech>
2022-11-20 14:31:44 +0700

    auth type

---
+++ src/types/auth.ts
@@ -0,0 +1,11 @@
+export interface IRequestLogin {
+    email: string;
+    password: string;
+}
+
+export interface IRequestRegister {
+    email: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+}
