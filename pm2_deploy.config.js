5dc4d26a8ba6de387a9cefee45ee5c33e994ddd6
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-11-20 23:47:35 +0700

    update build

---
+++ pm2_deploy.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [
+        {
+            name: 'water-park-api-dev',
+            script: './dist/index.js',
+            env_development: {
+                NODE_ENV: 'development',
+                PORT: 4001,
+                MONGO_URI:
+                    'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/DEV?retryWrites=true&w=majority',
+            },
+            env_uat: {
+                NODE_ENV: 'uat',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
