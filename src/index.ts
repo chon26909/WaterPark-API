7eaf0f8e3780b64197e614276aa9aaf1c106d823
Chonlasit <chonlasit.s@arise.tech>
2022-11-20 14:57:31 +0700

    add script build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Response } from 'express';
 import dotenv from 'dotenv';
 
 //routes
@@ -13,5 +13,5 @@ const PORT: number = Number(process.env.PORT) || 4000;
 connectMongoDB();
 app.use(express.json());
 app.use('/auth', authRoutes);
-app.get('/', (_, res) => res.status(200).json({ message: 'Hello World' }));
+app.get('/', (_, res: Response) => res.status(200).json({ message: 'Hello World' }));
 app.listen(PORT, () => console.log('http://localhost:' + PORT));
