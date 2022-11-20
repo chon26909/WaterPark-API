179c28e577edc57244c970dc6cf23080c9597db7
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-11-21 00:14:18 +0700

    update environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,21 @@ module.exports = {
                 PORT: 4001,
                 MONGO_URI:
                     'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/DEV?retryWrites=true&w=majority',
-                JWT_SECRET: waterpark,
+                JWT_SECRET: 'waterpark',
             },
             env_uat: {
                 NODE_ENV: 'uat',
+                PORT: 4002,
+                MONGO_URI:
+                    'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/UAT?retryWrites=true&w=majority',
+                JWT_SECRET: 'waterpark',
             },
             env_production: {
                 NODE_ENV: 'production',
+                PORT: 4003,
+                MONGO_URI:
+                    'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/PRODUCTION?retryWrites=true&w=majority',
+                JWT_SECRET: 'waterpark',
             },
         },
     ],
