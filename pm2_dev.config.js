d50d1c4b52b8e6391b4f31c17b67313c50fb23a6
Chonlasit <chonlasit.s@arise.tech>
2022-11-22 20:32:36 +0700

    update

--- pm2_dev.config.js
+++ pm2_dev.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
         {
             name: 'waterpark-api-dev',
-            script: '../dist/index.js',
-            env: {
+            script: './dist/index.js',
+            env_developemnt: {
                 PORT: 4002,
                 MONGO_URI:
                     'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/DEV?retryWrites=true&w=majority',
