897e7610eac0d9e727d23a0e32eff8c02ba1ab48
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-11-23 01:18:09 +0700

    watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'waterpark-api-dev',
             script: './dist/index.js',
-            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            watch: true,
             env_development: {
                 NODE_ENV: 'development',
                 PORT: 4002,
