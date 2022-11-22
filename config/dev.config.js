028adfb9b0bbee016af0ff57e88e4aef3ed7025f
Chonlasit <chonlasit.s@arise.tech>
2022-11-22 20:25:47 +0700

    up

--- config/dev.config.js
+++ config/dev.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'waterpark-api-dev',
             script: './dist/index.js',
-            env: {
+            env_development: {
                 PORT: 4002,
                 MONGO_URI:
                     'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/DEV?retryWrites=true&w=majority',
