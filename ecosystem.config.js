75fd2bfcabe291f1cd32d6ec7447e7ff1fb501f3
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-11-23 00:54:37 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,25 @@ module.exports = {
         {
             name: 'waterpark-api-dev',
             script: './dist/index.js',
-            env_developemnt: {
+            env_development: {
                 NODE_ENV: 'development',
                 PORT: 4002,
                 MONGO_URI:
                     'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/DEV?retryWrites=true&w=majority',
+                JWT_SECRET: 'waterpark',
+            },
+            env_uat: {
+                NODE_ENV: 'uat',
+                PORT: 4003,
+                MONGO_URI:
+                    'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/UAT?retryWrites=true&w=majority',
+                JWT_SECRET: 'waterpark',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 4001,
+                MONGO_URI:
+                    'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/PRODUCTION?retryWrites=true&w=majority',
                 JWT_SECRET: 'waterpark',
             },
         },
