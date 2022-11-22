7d05e85985b2e1a4dc18528b2a590ef3a7d359e1
Chonlasit <chonlasit.s@arise.tech>
2022-11-22 15:38:04 +0700

    update dev uat prod

---
+++ config/uat.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'waterpark-api-uat',
+            script: './dist/index.js',
+            env: {
+                PORT: 4003,
+                MONGO_URI:
+                    'mongodb+srv://water:1234@cluster0.aa3kc.mongodb.net/UAT?retryWrites=true&w=majority',
+                JWT_SECRET: 'waterpark',
+            },
+        },
+    ],
+};
